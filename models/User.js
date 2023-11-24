b80bf58c54b38e42e2b6e4464382b0110009ec82
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-24 13:08:31 -0800

    Add friends to User schema

--- models/User.js
+++ models/User.js
@@ -35,6 +35,12 @@ const userSchema = new Schema(
         ref: 'Thought',
       },
     ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ]
   },
   {
     toJSON: {
