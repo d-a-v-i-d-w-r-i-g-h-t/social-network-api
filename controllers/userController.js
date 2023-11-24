4825db7064c210e5d47b2f32116d7a5176a4504f
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-24 14:49:14 -0800

    Add userController functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,141 @@
-// getUsers
+const { User, Thought } = require('../models');
 
-// getSingleUser
+module.exports = {
 
-// createUser
+  // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res
+        .status(500)
+        .json(err)
+    }
+  },
+  
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+      
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user with that ID' });
+      }
+      res.json(user);
+    } catch (err) {
+      res
+        .status(500)
+        .json(err);
+    }
+  },
+  
+  // Create a user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json(err);
+    }
+  },
+  
+  // Update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.courseId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
 
-// updateUser
+      if (!course) {
+        return res
+          .status(404)
+          .json({ message: 'No user with this ID!'});
+      }
 
-// deleteUser
-// remove a user's associated thoughts when deleted
+      res.json(user);
+    } catch (err) {
+      res
+        .status(500)
+        .json(err);
+    }
+  },
+  
+  // Delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
 
-// addFriend
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID' });
+      }
 
-// deleteFriend
+      // remove a user's associated thoughts when deleted
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and thoughts deleted!' });
+    } catch (err) {
+      res
+        .status(500)
+        .json(err);
+    }
+  },
+  
+  // Add a friend to a user
+  async addFriend(req, res) {
+    try {
+      console.log('You are adding a friend');
+      console.log(req.body);
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res
+        .status(500)
+        .json(err);
+    }
+  },
+  
+  // Remove a friend from a user
+  async deleteFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friend: { friendId = req.params.friendId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return(res)
+          .status(404)
+          .json({ message: 'No user or friend found with provided IDs!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res
+        .status(500)
+        .json(err);
+    }
+  },
+}
+
