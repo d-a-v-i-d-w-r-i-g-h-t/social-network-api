bfa90770006b71d41f80d21465ed15e0e0f8f10d
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-24 13:53:36 -0800

    Add routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// for '/api' send to apiRoutes/index.js
+router.use('/api', apiRoutes);
+
+// for '/' or anything else, send an error message
+router.use((req, res) => res.send('Wront route!'));
+
+module.exports = router;
