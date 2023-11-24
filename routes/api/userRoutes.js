8b9bbdb99b8947d4623425d285f7c723a14e06c0
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-24 14:09:33 -0800

    Add friend routes to userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,6 +5,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  deleteFriend,
 } = require('../../controllers/userController.js');
 
 // /api/users
@@ -17,5 +19,10 @@ router.route('/:userId')
   .get(getSingleUser)
   .put(updateUser)
   .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
 
   module.exports = router;
